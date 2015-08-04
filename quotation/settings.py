f67cd52657126d1c12d22bfd3131fc1164fa092c
Diego Garcia <drgarcia1986@gmail.com>
2015-08-03 22:22:51 -0300

    add dolar australiano, peso, peso chileno and peso uruguaio to suported quotations and build a dynamic text with suported quotations to help command

--- quotation/settings.py
+++ quotation/settings.py
@@ -19,7 +19,17 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 REGEX_BASE = r'<input type="text" id="nacional" value="(?P<quote>[^"]+)"/>'
 QUOTATIONS = {
     'dolar': ('http://dolarhoje.com/', REGEX_BASE),
+    'dolar australiano': ('http://dolarhoje.com/australiano/', REGEX_BASE),
+    'dolar canadense': ('http://dolarhoje.com/canadense/', REGEX_BASE),
     'libra': ('http://librahoje.com/', REGEX_BASE),
     'euro': ('http://eurohoje.com/', REGEX_BASE),
-    'dolar canadense': ('http://dolarhoje.com/canadense', REGEX_BASE)
+    'peso': ('http://pesohoje.com/', REGEX_BASE),
+    'peso chileno': ('http://pesohoje.com/chileno/', REGEX_BASE),
+    'peso uruguaio': ('http://pesohoje.com/uruguaio/', REGEX_BASE)
 }
+
+QUOTATIONS_LIST = list(QUOTATIONS.keys())
+
+SUPORTED_QUOTATIONS = (
+    ' ,'.join(QUOTATIONS_LIST[:-1]) + ' e ' + QUOTATIONS_LIST[-1]
+)
